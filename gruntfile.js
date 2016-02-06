960adc66c9808c2d3992df724a69f3ace77c183d
Alexander Pape <a.pape@paneon.de>
2016-02-06 19:26:33 +0100

    Add browser_action for Login

--- gruntfile.js
+++ gruntfile.js
@@ -99,6 +99,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-contrib-sass");
     grunt.loadNpmTasks("grunt-shell");
