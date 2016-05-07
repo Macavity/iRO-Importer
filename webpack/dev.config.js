7e87409a140404a48eb32bf685d9864a659c0ae0
Alexander Pape <a.pape@paneon.de>
2016-05-08 00:01:24 +0200

    Remove unnecessary files and update crossbuilder files to commit 1a73914

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,58 +1,20 @@
 import path from 'path';
-import webpack from 'webpack';
 import baseConfig from './base.config';
-
-const port = 3000;
-const entry = [
-  `webpack-dev-server/client?http://localhost:${port}`,
-  'webpack/hot/only-dev-server'
-];
 const srcPath = path.join(__dirname, '../src/browser/');
 
-const config = baseConfig({
+export default baseConfig({
   input: {
-    background: [`${srcPath}extension/background/index`, ...entry],
-    window: [`${srcPath}window/index`, ...entry],
-    popup: [`${srcPath}extension/popup/index`, ...entry],
-    inject: [`${srcPath}extension/inject/index`, ...entry],
-    app: [path.join(__dirname, '../src/web/index'), ...entry]
+    background: [`${srcPath}extension/background/`],
+    window: [`${srcPath}window/`],
+    popup: [`${srcPath}extension/popup/`],
+    inject: [`${srcPath}extension/inject/`]
   },
   output: {
-    path: path.join(__dirname, '../dev/js'),
-    publicPath: `http://localhost:${port}/js/`
+    path: path.join(__dirname, '../build/extension')
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  loaders: [{
-    test: /\.js$/,
-    loader: 'babel',
-    exclude: /node_modules/,
-    query: {
-      presets: ['es2015', 'stage-0', 'react'],
-      plugins: [
-        'add-module-exports',
-        'transform-decorators-legacy',
-        [
-          'react-transform',
-          {
-            transforms: [{
-              transform: 'react-transform-hmr',
-              imports: ['react'],
-              locals: ['module']
-            }, {
-              transform: 'react-transform-catch-errors',
-              imports: ['react', 'redbox-react']
-            }]
-          }
-        ]
-      ]
+  globals: {
+    'process.env': {
+      NODE_ENV: '"production"'
     }
-  }],
-  entry
+  }
 });
-
-config.devtool = 'eval';
-
-export default config;
